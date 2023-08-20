ac76f9e93fd22f04d7ab524b9dc754925b984443
Nalier2002 <nalieskin@gmail.com>
2023-08-20 21:33:15 +0100

    Initial commit

---
+++ Calculator.py
@@ -0,0 +1,34 @@
+
+
+def add(n1, n2):
+  return n1 + n2
+
+def subtract(n1, n2):
+  return n1 - n2
+
+def multiply(n1, n2):
+  return n1 * n2
+
+def divide(n1, n2):
+  return n1 / n2
+
+operations = {
+  "+": add, 
+  "-": subtract, 
+  "*": multiply, 
+  "/": divide
+}
+
+num1 = int(input("What is the first number?: "))
+num2 = int(input("What is the second number?: "))
+for symbol in operations:
+    print(symbol)
+
+operation_symbol = input("Pick an operation from the above: ")
+
+
+if operation_symbol in operations:
+    answer = operations[operation_symbol](num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+else:
+    print("Invalid operation symbol. Please try again.")
